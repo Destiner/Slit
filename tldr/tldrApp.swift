ebfe2c80b472046e3c1d470508f14acd62f85201
Timur Badretdinov <4247901+Destiner@users.noreply.github.com>
2026-01-18 11:51:12 +0000

    Initial Commit

---
+++ tldr/tldrApp.swift
@@ -0,0 +1,17 @@
+//
+//  tldrApp.swift
+//  tldr
+//
+//  Created by Timur Badretdinov on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct tldrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
